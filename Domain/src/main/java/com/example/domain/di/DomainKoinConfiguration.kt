ca1e8f15681e881e9155a3dca209822b364ef521
fgarcgar <francisco.garcia.garzon@everis.com>
2020-02-19 16:32:01 +0100

    application class updated,check PresentationKoinConfiguration!!!!

--- Domain/src/main/java/com/example/domain/di/DomainKoinConfiguration.kt
+++ Domain/src/main/java/com/example/domain/di/DomainKoinConfiguration.kt
@@ -3,6 +3,7 @@ package com.example.domain.di
 import com.example.domain.useCases.GetTeam
 import org.koin.dsl.module
 
+
 class DomainKoinConfiguration {
     fun getModule() = module {
         factory { GetTeam(get()) }
--- app/src/main/java/com/example/formacionandroid/TeamsApplication.kt
+++ app/src/main/java/com/example/formacionandroid/TeamsApplication.kt
@@ -1,18 +1,21 @@
 package com.example.formacionandroid
 
 import android.app.Application
+import android.util.Log
+import com.example.common.TAG
 import com.example.data.di.DataKoinConfiguration
 import com.example.domain.di.DomainKoinConfiguration
 import com.example.formacionandroid.di.PresentationKoinConfiguration
-
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class TeamsApplication :  Application() {
+class TeamsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
+        Log.d(TAG, "TeamsApplication.onCreate")
         startKoin {
+            androidContext(this@TeamsApplication)
             modules(
                 listOf(
                     DataKoinConfiguration().getModule(),
